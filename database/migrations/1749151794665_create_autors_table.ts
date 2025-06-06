f81e4ad8e0d4ab822ffadf0e8e45e6ac7eabe8af
Danithza <ndlarrotta01@gmail.com>
2025-06-06 07:42:36 -0500

    cambio de dato en migraciones y modelos

--- database/migrations/1749151794665_create_autors_table.ts
+++ database/migrations/1749151794665_create_autors_table.ts
@@ -7,7 +7,7 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.string('nombre').notNullable()
-      table.string('fecha_nacimiento').notNullable()
+      table.date('fecha_nacimiento').notNullable()
       table.timestamp('created_at')
       table.timestamp('updated_at')
     })
